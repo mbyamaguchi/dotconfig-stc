00fdc91cf26df45c9c0f8cf36bcff62cb16f9df9
mbyamaguchi <hayama.hika@gmail.com>
2026-05-23 17:00:33 +0900

    Update zshrc

--- nvim/lsp/cpp.lua
+++ nvim/lsp/cpp.lua
@@ -1,5 +1,17 @@
 return {
-	cmd = { "clangd", "--background-index", "--clang-tidy" },
-	filetypes = { "c", "cpp", "objc", "objcpp" },
-	root_markers = { "CMakeLists.txt", "compile_commands.json", ".git" },
+    cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--completion-style=detailed",
+        "--header-insertion=iwyu",
+        "--fallback-style=llvm",
+    },
+    filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+    root_markers = {
+        "compile_commands.json",
+        "compile_flags.txt",
+        ".clangd",
+        ".git",
+    }
 }
