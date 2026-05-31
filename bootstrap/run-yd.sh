7718b161d019f5eda8039552061ea67d0530ef5e
mbyamaguchi <hayama.hika@gmail.com>
2026-06-01 06:24:57 +0900

    Update run-yd

--- bootstrap/run-yd.sh
+++ bootstrap/run-yd.sh
@@ -6,6 +6,7 @@ yt-dlp \
   --merge-output-format mp4 \
   -o "$(playlist_index)04d_%(title)s.%(ext)s" \
   --download-archive archive.txt \
+  --restrict-filenames \
   --embed-metadata --embed-thumbnail \
   --sleep-interval 5 --max-sleep-interval 15 \
   --retries 10 --fragment-retries 10 \
