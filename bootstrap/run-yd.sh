3650ed2e8202eb9973cd7332a6137412adaaac33
mbyamaguchi <hayama.hika@gmail.com>
2026-06-01 06:23:41 +0900

    Add yt-dlp

---
+++ bootstrap/run-yd.sh
@@ -0,0 +1,13 @@
+echo "download the playlist:"
+echo ${URL}
+
+yt-dlp \
+  -f "bv*+ba[ext=m4a]/bv*+ba/b" \
+  --merge-output-format mp4 \
+  -o "$(playlist_index)04d_%(title)s.%(ext)s" \
+  --download-archive archive.txt \
+  --embed-metadata --embed-thumbnail \
+  --sleep-interval 5 --max-sleep-interval 15 \
+  --retries 10 --fragment-retries 10 \
+  --ignore-errors \
+  ${URL}
---
+++ bootstrap/setup-ytdlp.sh
@@ -0,0 +1,6 @@
+sudo apt install ffmpeg -y
+
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ~/.local/bin/yt-dlp
+chmod a+rx $HOME/.local/bin/yt-dlp
+
+
