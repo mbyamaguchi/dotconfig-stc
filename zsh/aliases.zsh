76de003056afe842cedf5201675a888b2c3caa87
Hikaru Yamaguchi <161670081+mbyamaguchi@users.noreply.github.com>
2026-06-27 09:13:57 +0900

    Refactor zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,28 @@
+# editor
+alias v='bob run stable'
+
+# ls
+alias ls='eza'
+alias ll='eza -l'
+alias la='eza -A'
+alias lla='eza -lA'
+
+# git
+alias g='git'
+alias gst='git status'
+alias gsw='git switch'
+alias gbr='git branch'
+alias gfe='git fetch'
+alias gpl='git pull'
+alias gad='git add'
+alias gada='git add .'
+alias gcm='git commit'
+alias gmg='git merge'
+alias gpsh='git push'
+
+# docker
+alias d='docker'
+alias dc='docker compose'
+
+# cargo
+alias cgo='cargo'
